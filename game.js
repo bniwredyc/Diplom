e5141d00c446701c841e2095f1dccd672d5429e9
Alex Nituleac <nituleacal@gmail.com>
2017-05-22 23:42:24 +0300

    added 3 classes

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ class Vector {
     this.x = x;
     this.y = y;
   }
+
   plus(v){
     if (v instanceof Vector) {
      return new Vector(this.x + v.x, this.y + v.y)
@@ -66,17 +67,43 @@ class Actor {
 
   isIntersect(item) {
     if (item instanceof Actor && arguments.length != 0) {
-      if (item === item) {
+      if (item.left === this.left && item.top === this.top && item.right === this.right && item.bottom === this.bottom) {
         return false;
       }
-      // if (item.left >= this.left || item.top >= this.top) {
-      //   return false;
-      // }
-      // if (item.left == this.left && item.top == this.top && item.right == this.right && item.bottom == this.bottom) {
-      //   return false;
-      // }
-    } else{
+      if (item.right < this.left || item.top > this.bottom || item.left > this.right || item.bottom < this.top) {
+        return false;
+      }
+      if (item.right === this.left || item.left === this.right || item.top === this.bottom || item.bottom === this.top) {
+        return false;
+      }
+      if (-item.left === this.left && -item.top === this.top && -item.right === this.right && -item.bottom === this.bottom) {
+        return false;
+      }
+      if (item.left > this.left && item.top > this.top && item.right < this.right && item.bottom < this.bottom) {
+        return true;
+      }
+      if (item.left < this.right && item.right > this.left && item.top < this.bottom && item.bottom > this.top) {
+        return true;
+      }
+
+    } else {
       throw new Error('Object must to be an instance of Actor');
     }
+  }
+}
+
+class Level {
+  constructor(grid = [], actors = []) {
+    this.grid = grid;
+    this.actors = actors;
+    this.height = grid.length;
+    this.width = grid[0].length;
+    this.status = null;
+    this.finishDelay = 1;
+
+  }
+
+  get player () {
+    return this.actors.filter((actor) => actor.type === 'player')[0];
   }
 }
